18a8eaecc14a280aecb8a518c555fe3141bbe3bb
dtcpay-gorden <gorden.lim@dtcpay.com>
2025-05-29 12:04:12 +0800

    UPDATE: test

--- api/tina/backend.ts
+++ api/tina/backend.ts
@@ -2,23 +2,26 @@ import { TinaNodeBackend, LocalBackendAuthProvider } from '@tinacms/datalayer';
 import { TinaAuthJSOptions, AuthJsBackendAuthProvider } from 'tinacms-authjs';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-import databaseClient from '../../tina/__generated__/databaseClient.js';
-
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true';
 
-const tinaBackend = TinaNodeBackend({
-  authProvider: isLocal
-    ? LocalBackendAuthProvider()
-    : AuthJsBackendAuthProvider({
-        authOptions: TinaAuthJSOptions({
-          databaseClient: databaseClient,
-          secret: process.env.NEXTAUTH_SECRET!,
-        }),
-      }),
-  databaseClient,
-});
+async function createTinaBackend() {
+  const { default: databaseClient } = await import('../../tina/__generated__/databaseClient.js');
 
-function handler(req: VercelRequest, res: VercelResponse) {
+  return TinaNodeBackend({
+    authProvider: isLocal
+      ? LocalBackendAuthProvider()
+      : AuthJsBackendAuthProvider({
+          authOptions: TinaAuthJSOptions({
+            databaseClient: databaseClient,
+            secret: process.env.NEXTAUTH_SECRET!,
+          }),
+        }),
+    databaseClient,
+  });
+}
+
+async function handler(req: VercelRequest, res: VercelResponse) {
+  const tinaBackend = await createTinaBackend();
   return tinaBackend(req, res);
 }
 
